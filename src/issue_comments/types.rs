1f8f0b3ec9e4da04592d1331a13840fe32796ec9
Alex McArther <acmcarther@gmail.com>
2015-08-24 21:21:10 -0700

    Issue comments urls

--- src/issue_comments/types.rs
+++ src/issue_comments/types.rs
@@ -1,6 +1,9 @@
 pub use self::types::{
+  IssueId,
+  CommentId
 };
 
 mod types {
-
+  pub type IssueId = u32;
+  pub type CommentId = u32;
 }
---
+++ src/issue_comments/url_builders.rs
@@ -0,0 +1,79 @@
+pub use self::url_builders::{
+  issue_comments,
+  issue_comments_for_repo,
+  issue_comment_at,
+};
+
+
+mod url_builders {
+  use types::{
+    Repository,
+    Url,
+  };
+
+  use issue_comments::types::{
+    IssueId,
+    CommentId
+  };
+
+  #[allow(dead_code)]
+  pub fn issue_comments(repo: &Repository, issue_id: &IssueId) -> Url {
+    "https://api.github.com/repos/".to_owned() +
+      &repo.owner + "/" +
+      &repo.repo_name + "/issues/" +
+      &issue_id.to_string() + "/comments"
+  }
+
+  pub fn issue_comments_for_repo(repo: &Repository) -> Url {
+    "https://api.github.com/repos/".to_owned() +
+      &repo.owner + "/" +
+      &repo.repo_name + "/issues/comments"
+  }
+
+  #[allow(dead_code)]
+  pub fn issue_comment_at(repo: &Repository, comment_id: &CommentId) -> Url {
+    issue_comments_for_repo(repo) + "/" + &comment_id.to_string()
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use expectest::core::expect;
+    use expectest::matchers::be_equal_to;
+
+    pub use types::{
+      Repository,
+      Url,
+    };
+
+    pub use super::{
+      issue_comments,
+      issue_comments_for_repo,
+      issue_comment_at,
+    };
+
+    #[test]
+    fn it_builds_issue_comments() {
+      let repo = Repository { owner: "test_owner".to_owned(), repo_name: "test_repo".to_owned() };
+      let issue_id = 21;
+      let expected = "https://api.github.com/repos/test_owner/test_repo/issues/21/comments".to_owned();
+      expect!(issue_comments(&repo, &issue_id)).to(be_equal_to(expected));
+    }
+
+    #[test]
+    fn it_builds_issue_comments_for_repo() {
+      let repo = Repository { owner: "test_owner".to_owned(), repo_name: "test_repo".to_owned() };
+      let expected = "https://api.github.com/repos/test_owner/test_repo/issues/comments".to_owned();
+      expect!(issue_comments_for_repo(&repo)).to(be_equal_to(expected));
+    }
+
+    #[test]
+    fn it_builds_issue_comment_at() {
+      let repo = Repository { owner: "test_owner".to_owned(), repo_name: "test_repo".to_owned() };
+      let comment_id = 21;
+      let expected = "https://api.github.com/repos/test_owner/test_repo/issues/comments/21".to_owned();
+      expect!(issue_comment_at(&repo, &comment_id)).to(be_equal_to(expected));
+    }
+  }
+}
+
+
